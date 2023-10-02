6b5ca36203167c1ee19f6a750bf4919e386dfa13
nick thomas <nichoth@gmail.com>
2023-10-01 20:10:59 -0700

    test import works

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 import { test } from '@socketsupply/tapzero'
-import { example } from '../dist/index.js'
+import { example } from '../src/index.js'
 
 test('example', async t => {
     t.ok('ok', 'should be an example')
